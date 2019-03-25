508d130d51448ee71fca6be3101e307590e5d7fa
Royaaaaaa <43308173+Royaaaaaa@users.noreply.github.com>
2019-03-25 14:59:20 +1100

    commit

--- src/test/java/rockets/model/UserUnitTest.java
+++ src/test/java/rockets/model/UserUnitTest.java
@@ -92,10 +92,10 @@ public class UserUnitTest {
 
     @DisplayName("should throw exception when the password has right pattern.")
     @ParameterizedTest
-    @ValueSource(strings = {"aaZZa44@"})
+    @ValueSource(strings = {"aaZZa88884@"})
     public void shouldThrowExceptionWhenSetPasswordToRightPattern(String password) {
-        String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
-        password.matches(pattern);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> target.setPassword(password));
+        assertEquals("Invalid password", exception.getMessage());
     }
 
 
